8fb49330c755e2195cc7e4e6ed2f98bb6bffd86b
liskovich <ricardsliskovskis@gmail.com>
2023-06-04 18:39:39 +0300

    temp user retrieval

--- web/src/routes/upload/+server.ts
+++ web/src/routes/upload/+server.ts
@@ -7,7 +7,10 @@ export async function POST(event) {
 		process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 	}
 
-	const response = await fetch(`${PUBLIC_BACKEND_URL}/upload`, {
+	// TODO: remove in PROD
+	var userId = event.locals.user?.id;
+
+	const response = await fetch(`${PUBLIC_BACKEND_URL}/upload/${userId}`, {
 		method: 'POST',
 		mode: 'cors',
 		body: await event.request.formData()
